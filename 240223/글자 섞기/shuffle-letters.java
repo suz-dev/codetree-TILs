9e14d5bd0c841c79972126347407b75dfc24fa51
Sujeong Jo <suz.dev33@gmail.com>
2024-02-23 08:52:29 +0900

    Title: 글자 섞기, Time: 1820ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240223/글자 섞기/shuffle-letters.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String[][] arr = new String[2][n];
+
+        for(int i = 0; i < n; i++){
+            String str = br.readLine();
+            ArrayList<Character> tmp = new ArrayList();
+            for(int j = 0; j < str.length(); j++){
+                tmp.add(str.charAt(j));
+            }
+
+            Collections.sort(tmp);
+            StringBuilder sb = new StringBuilder();
+            for(int j = 0; j < str.length(); j++){
+                sb.append(tmp.get(j));
+            }
+
+            arr[0][i] = sb.toString();  // 알파벳 오름차순
+            arr[1][i] = sb.reverse().toString();    // 알파벳 내림차순
+        }
+
+        int idx = 0;
+        while(idx < n){
+            String now = arr[0][idx];
+            String reverseNow = arr[1][idx];
+
+            ArrayList<String> list1 = new ArrayList<>();
+            ArrayList<String> list2 = new ArrayList<>();
+      
+            for(int i = 0; i < n; i++){
+                if(i != idx && now.compareTo(arr[1][i]) > 0) {
+                    list1.add(arr[1][i]);
+                }
+            }
+
+            for(int i = 0; i < n; i++){
+                if(i != idx && reverseNow.compareTo(arr[0][i]) > 0) {
+                    list2.add(arr[0][i]);
+                }
+            }
+
+            System.out.println((list1.size() + 1) + " " + (list2.size()+1));
+            idx++;
+        }
+    }
+}
